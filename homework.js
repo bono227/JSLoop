b5f10df06e56faf328ad47388ced1b5040a428ac
bono227 <dwctab@gmail.com>
2024-07-16 23:53:05 -0500

    Loop Homework v1

---
+++ homework.js
@@ -0,0 +1,22 @@
+// task without Math
+
+
+let i = 1;
+while (i <= 100) {
+    if (i === 50) {
+        console.log("Half Way There!")
+    } else if (i ===100) {
+        console.log("You Made it!")
+    } else if (i % 10 === 0) {
+        console.log("Checkpoint! " + i)
+    }
+    i++;
+}
+
+
+let o = 1;
+while (o <=100) {
+    console.log(o);
+    o = o+2;
+}
+     
